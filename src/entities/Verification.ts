33213d16ede8fb16aa90eb6f5bbb5ddabf037f12
preciouStar <seongjin605@users.noreply.github.com>
2020-02-25 00:57:01 +0900

    Add entities of ride etc

--- src/entities/Verification.ts
+++ src/entities/Verification.ts
@@ -5,9 +5,11 @@ import {
     Entity,
     PrimaryGeneratedColumn,
     UpdateDateColumn,
-    BeforeInsert
+    BeforeInsert,
+    ManyToOne
 } from 'typeorm';
 import { verificationTarget } from 'src/types/types';
+import User from './User';
 
 const PHONE = 'PHONE';
 const EMAIL = 'EMAIL';
@@ -28,6 +30,12 @@ class Verification extends BaseEntity {
 
     @Column({ type: 'boolean', default: false })
     used: boolean;
+
+    @ManyToOne(
+        type => User,
+        user => user.verifications
+    )
+    user: User;
 
     @CreateDateColumn()
     createdAt: string;
