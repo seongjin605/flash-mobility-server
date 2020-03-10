7e7a128b28fd0ac34924a1a338e36db76c292f8c
preciouStar <seongjin605@users.noreply.github.com>
2020-03-11 00:44:02 +0900

    Update words properly

--- src/utils/privateResolver.ts
+++ src/utils/privateResolver.ts
@@ -5,7 +5,7 @@ const privateResolver = resolverFunction => async (
     info
 ) => {
     if (!context.req.user) {
-        throw new Error('No jWT. Refuse to proceed');
+        throw new Error('No JWT. Refuse to proceed');
     }
     const resolved = await resolverFunction(parent, args, context, info);
     return resolved;
