6a7771ebbea063ec0baaefb97fd087fb32b2980b
preciouStar <seongjin605@users.noreply.github.com>
2020-03-13 22:06:10 +0900

    Set mailgun-js for email API

--- src/utils/privateResolver.ts
+++ src/utils/privateResolver.ts
@@ -8,6 +8,10 @@ const privateResolver = resolverFunction => async (
         throw new Error('No JWT. Refuse to proceed');
     }
     const resolved = await resolverFunction(parent, args, context, info);
+    console.log('parent:', parent);
+    console.log('args:', args);
+    console.log('context:', context);
+    console.log('info:', info);
     return resolved;
 };
 
