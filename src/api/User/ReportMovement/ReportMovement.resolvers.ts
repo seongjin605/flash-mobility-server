2d57daf55e94ad71955c9144b9d2ca739d6c3627
preciouStar <qkrtjdwls605@naver.com>
2020-05-08 00:41:12 +0900

    Create ReportMovement Resolver (#32)
    * If user want to change password, then update user's password
    
    * Update README.md
    
    * Create ToggleDrivingMode resolver
    
    * Create ReportMovement Resolver
    
    Co-authored-by: preciouStar <seongjin605@users.noreply.github.com>

--- src/api/User/ReportMovement/ReportMovement.resolvers.ts
+++ src/api/User/ReportMovement/ReportMovement.resolvers.ts
@@ -1,27 +1,23 @@
 import {
-    UpdateMyProfileMutationArgs,
-    UpdateMyProfileResponse
+    ReportMovementMutationArgs,
+    ReportMovementResponse
 } from './../../../types/graph.d';
-import { Resolvers } from './../../../types/resolvers';
+import { Resolvers } from '../../../types/resolvers';
 import privateResolver from '../../../utils/privateResolver';
 import User from '../../../entities/User';
 import cleanNullArgs from '../../../utils/cleanNullArgs';
 
 const resolvers: Resolvers = {
     Mutation: {
-        UpdateMyProfile: privateResolver(
+        ReportMovement: privateResolver(
             async (
                 _,
-                args: UpdateMyProfileMutationArgs,
+                args: ReportMovementMutationArgs,
                 { req }
-            ): Promise<UpdateMyProfileResponse> => {
+            ): Promise<ReportMovementResponse> => {
                 const user: User = req.user;
                 const notNull = cleanNullArgs(args);
                 try {
-                    if (args.password !== null) {
-                        user.password = args.password;
-                        user.save();
-                    }
                     await User.update({ id: user.id }, { ...notNull });
                     return {
                         ok: true,
