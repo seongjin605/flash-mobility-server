3ef135fd6828100809b51d5426b77df997721e3f
preciouStar <seongjin605@users.noreply.github.com>
2020-05-19 00:09:14 +0900

    Authenticating WebSocket Subscriptions part one

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,33 @@ import { Options } from 'graphql-yoga';
 import { createConnection } from 'typeorm';
 import app from './app';
 import connectionOptions from './ormConfig';
+import decodeJWT from './utils/decodeJWT';
 
 const PORT: number | string = process.env.PORT || 4000;
 const PLAYGROUND_ENDPOINT: string = '/playground';
 const GRAPHQL_ENDPOINT: string = '/graphql';
+const SUBSCRIPTION_ENDPOINT: string = '/subscription';
 
 const appOptions: Options = {
     port: PORT,
     playground: PLAYGROUND_ENDPOINT,
-    endpoint: GRAPHQL_ENDPOINT
+    endpoint: GRAPHQL_ENDPOINT,
+    subscriptions: {
+        path: SUBSCRIPTION_ENDPOINT,
+        onConnect: async (connectionParams) => {
+            const token = connectionParams['X-JWT'];
+            if (token) {
+                const user = await decodeJWT(token);
+                console.log(user);
+                if (user) {
+                    return {
+                        currentUser: user
+                    };
+                }
+            }
+            throw new Error('NO JWT. can not subscribe');
+        }
+    }
 };
 
 const handleAppStart = () => console.log(`Listening on port:${PORT}`);
@@ -21,4 +39,4 @@ createConnection(connectionOptions)
     .then(() => {
         app.start(appOptions, handleAppStart);
     })
-    .catch(error => console.log(error));
+    .catch((error) => console.log(error));
